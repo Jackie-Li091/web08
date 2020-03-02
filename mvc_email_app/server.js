65977292396334d15d181560dbb702d2d6e877d0
Jackie-Li091 <jackienleee@gmail.com>
2020-03-01 20:16:50 -0500

    models

--- mvc_email_app/server.js
+++ mvc_email_app/server.js
@@ -15,19 +15,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static('public'));
 
+//load product model
+const productModel = require("./models/product");
 
 const PORT = 3000;
 
 //home route
 app.get("/",(req,res)=>{
 
-    res.render("general/home");
+    res.render("general/home",{
+        title : "home page"
+    });
 });
 
 //contact us route
 app.get("/contact-us",(req,res)=>{
 
-    res.render("general/contactUs");
+    res.render("general/contactUs",{
+        title : "contact-us page"
+    });
 });
 
 //user submit the request
@@ -39,13 +45,18 @@ app.post("/contact-us",(req,res)=>{
 //show all product
 app.get("/product/list",(req,res)=>{
 
-    res.render("products/productList");
+    res.render("products/productList",{
+        title : "product list page",
+        products : productModel.getAllProducts()
+    });
 });
 
 //show add product form
 app.get("/product/add",(req,res)=>{
 
-    res.render("products/productAdd");
+    res.render("products/productAdd",{
+        title : "add product page"
+    });
 });
 
 //user submit the add product form
